0aa56e590bc65506ff11b41f88ad09f9b0b5d061
ignoremyaccount <jw90457@leisd.net>
2025-08-13 14:51:28 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+/*
+hope this code works, considering the C LSP for github is limited 
+i have no way of knowing the function declarations without looking
+at the header online
+*/
+
+/*
+implement: 
+half censorship 
+full censorship 
+first letter only
+
+*/
+
 /* the blacklist */
 #define __lprof_num_strings_dev 12 
 char **lprof_init_bl(int fd) {
@@ -19,5 +33,5 @@ char **lprof_init_bl(int fd) {
 
 /* l profane - c style strings (null charred) */
 void lprof_cs(char *word, char **bl) {
-  
+  int _s_len = strlen(word); 
 }
